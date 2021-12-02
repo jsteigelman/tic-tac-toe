0b53243e38de811ee6e608a67a46767897ab875b
Joey Steigelman <joey.steigelman@gmail.com>
2021-12-02 13:55:19 -0500

    added functionality for placing game piece on board with click event

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const X_CLASS = 'x'
 const O_CLASS = 'o'
 
-const boardCells = document.querySelectorAll('.cell');
-let xTurn
+const boardCells = document.querySelectorAll('.cell')
+let xTurn = true
 
 const handleClick = (e) => {
-    console.log("clicked!")
-    const currentClass = xTurn ? X_CLASS : O_CLASS
-    // place game piece
+    console.log("cell clicked!")
+    const currentCell = e.target
+    const currentPlayer = xTurn ? X_CLASS : O_CLASS
+    placeGamePiece(currentCell, currentPlayer)
     // check for win
     // check for tie
     // change turns
@@ -17,3 +18,6 @@ boardCells.forEach((cell) => {
     cell.addEventListener('click', handleClick, { once: true })
 })
 
+const placeGamePiece = (cell, currentPlayer) => {
+    cell.classList.add(currentPlayer)
+}
