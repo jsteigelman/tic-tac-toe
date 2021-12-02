7aa8c8a7b0dc489e8e116874d7a548fcd2a5aa1d
Joey Steigelman <joey.steigelman@gmail.com>
2021-12-02 14:07:22 -0500

    created js file for game functions and move functions from game file to game-functions file

--- game.js
+++ game.js
@@ -7,43 +7,9 @@ let xTurn
 
 startGame()
 
-const startGame = () => {
-    xTurn = true
-
-    boardCells.forEach((cell) => {
-        cell.addEventListener('click', handleClick, { once: true })
-    })
-
-    setHoverPiece()
-
-}
-
-const handleClick = (e) => {
-    console.log("cell clicked!")
-    const currentCell = e.target
-    const currentPlayer = xTurn ? X_CLASS : O_CLASS
-    placeGamePiece(currentCell, currentPlayer)
-    // check for win
-    // check for tie
-    nextTurn()
-}
 
 
 
-const placeGamePiece = (cell, currentPlayer) => {
-    cell.classList.add(currentPlayer)
-}
 
-const nextTurn = () => {
-    xTurn = !xTurn
-}
 
-const setHoverPiece = () => {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(O_CLASS)
-    if (xTurn) {
-        board.classList.add(X_CLASS)
-    } else {
-        board.classList.add(O_CLASS)
-    }
-}
+
